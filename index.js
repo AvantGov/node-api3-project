846e670c028669eb4a9caf59e2d33ee33d80413f
Alex <alexcouts18@gmail.com>
2020-08-10 22:09:03 -0500

    server boilerplate added to index.js, initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require("express")
+
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
